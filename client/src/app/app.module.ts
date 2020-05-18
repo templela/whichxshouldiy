f0c9f2365c11f3727b89b9a18e5e8017dee111eb
lawrence <lawrence.temple@gmail.com>
2020-05-18 01:54:27 -0400

    Added simple mat table

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnimelistComponent } from './animelist/animelist.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { CdkTableModule } from '@angular/cdk/table';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +18,10 @@ import { AnimelistComponent } from './animelist/animelist.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    CdkTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
